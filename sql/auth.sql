d24b2149302d6e42eb9f9c732d755bca106d20fb
Tiago Ilieve <tiago.myhro@gmail.com>
2020-02-10 18:47:33 -0300

    Add 'auth' table

---
+++ sql/auth.sql
@@ -0,0 +1,11 @@
+-- name: add-user
+INSERT INTO auth (email, password)
+  VALUES ($1, crypt($2, gen_salt('bf')))
+;
+
+-- name: check-password
+SELECT id
+  FROM auth
+  WHERE email = $1
+    AND password = crypt($2, password)
+;
