0a4655f0c0853e1f51ff6d53facbcaa0ab3e8a5e
Tiago Ilieve <tiago.myhro@gmail.com>
2020-02-11 21:19:11 -0300

    updater: make it continuous

--- cmd/updater/main.go
+++ cmd/updater/main.go
@@ -4,27 +4,36 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/myhro/ovh-checker/postgres"
 	"github.com/nleof/goyesql"
 )
 
-func main() {
+func fetchAPI() (string, error) {
 	url := "https://www.ovh.com/engine/api/dedicated/server/availabilities?country=pt"
-
-	db, err := postgres.New()
-	if err != nil {
-		log.Fatal(err)
-	}
 
 	log.Print("Fetching servers availability")
 	resp, err := http.Get(url)
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
 	log.Print("Done")
 
 	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(body), nil
+}
+
+func sleep() {
+	time.Sleep(60 * time.Second)
+}
+
+func main() {
+	db, err := postgres.New()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -34,10 +43,23 @@ func main() {
 		log.Fatal(err)
 	}
 
-	log.Print("Updating servers offers")
-	_, err = db.Exec(queries["import-json"], string(body))
-	if err != nil {
-		log.Fatal(err)
+	for {
+		body, err := fetchAPI()
+		if err != nil {
+			log.Print(err)
+			sleep()
+			continue
+		}
+
+		log.Print("Updating servers offers")
+		_, err = db.Exec(queries["import-json"], body)
+		if err != nil {
+			log.Print(err)
+			sleep()
+			continue
+		}
+		log.Print("Done")
+
+		sleep()
 	}
-	log.Print("Done")
 }
