741db6d9bfd7c59f6c88a21a4b14e79ea2c4464e
Tiago Ilieve <tiago.myhro@gmail.com>
2020-02-22 23:45:57 -0300

    errors: add New() and UnauthorizedWithMessage()
    Also drop unused BadRequest() and ensure all errors will prevent
    following handlers from being executed.

--- api/errors/errors.go
+++ api/errors/errors.go
@@ -1,16 +1,12 @@
 package errors
 
 import (
+	"errors"
 	"net/http"
 	"strings"
 
 	"github.com/gin-gonic/gin"
 )
-
-// BadRequest returns a regular Bad Request response
-func BadRequest(c *gin.Context) {
-	c.String(http.StatusBadRequest, "Bad Request")
-}
 
 // BadRequestWithMessage returns a Bad Request response with a custom error message
 func BadRequestWithMessage(c *gin.Context, msg string) {
@@ -18,12 +14,27 @@ func BadRequestWithMessage(c *gin.Context, msg string) {
 		"error": msg,
 	}
 
-	c.JSON(http.StatusBadRequest, body)
+	c.AbortWithStatusJSON(http.StatusBadRequest, body)
+}
+
+// New returns a standard error
+func New(e string) error {
+	return errors.New(e)
 }
 
 // InternalServerError returns a regular Internal Server Error response
 func InternalServerError(c *gin.Context) {
 	c.String(http.StatusInternalServerError, "Internal Server Error")
+	c.Abort()
+}
+
+// UnauthorizedWithMessage returns an Unauthorized response with a custom error message
+func UnauthorizedWithMessage(c *gin.Context, msg string) {
+	body := gin.H{
+		"error": msg,
+	}
+
+	c.AbortWithStatusJSON(http.StatusUnauthorized, body)
 }
 
 // ValidationMessage turns a validation error into a more user-friendly message
